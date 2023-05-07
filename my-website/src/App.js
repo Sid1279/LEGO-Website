9120b14c3cbe01efd26b091343e184c85b0a90d3
Siddharth <siddharth.n.viswanath@gmail.com>
2023-05-07 13:50:16 -0400

    First commit frfr

---
+++ my-website/src/App.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react'
+
+import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from "./containers";
+import {CTA, Brand, Navbar} from "./components";
+import "./App.css";
+
+const App = () => {
+  useEffect(() => {
+    document.title = "GPT3"
+  }, []);
+  return (
+    <div className = "App">
+        <div className = "gradient__bg">
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
