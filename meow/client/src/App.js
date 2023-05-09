66e27e8b48c3e3173a18df5f564d7ecc032b7bbb
Siddharth <siddharth.n.viswanath@gmail.com>
2023-05-09 01:11:49 -0400

    pushed meow #2 iteration
    lays ketchup sucks

--- meow/client/src/App.js
+++ meow/client/src/App.js
@@ -1,21 +1,19 @@
-import Home from "./components/Home"
-import Login from "./components/Login"
-import Signup from "./components/Signup"
-
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { useState } from 'react';
+import { Route, Routes, Navigate } from "react-router-dom";
+import Main from "./components/Main";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
-return (
-  <div className="App">
-  <Router>
-    <Routes>
-      <Route path="/" element={<Login/>}/>
-      <Route path="/signup" element={<Signup/>}/>
-      <Route path="/home" element={<Home/>}/>
-    </Routes>
-  </Router>
-  </div>
-  );
+	const user = localStorage.getItem("token");
+
+	return (
+		<Routes>
+			{user && <Route path="/" exact element={<Main />} />}
+			<Route path="/signup" exact element={<Signup />} />
+			<Route path="/login" exact element={<Login />} />
+			<Route path="/" element={<Navigate replace to="/login" />} />
+		</Routes>
+	);
 }
+
 export default App;
